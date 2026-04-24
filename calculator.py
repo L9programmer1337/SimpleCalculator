898a928017c8ef244a7184c64b448cc9540e6672
L9programmer1337 <thomastailor03@gmail.com>
2026-04-24 14:08:02 +0200

    Implement loop for continuous calculations
    Added a loop to allow multiple calculations and restart option.

--- calculator.py
+++ calculator.py
@@ -1,22 +1,41 @@
-calc1 = float(input("Number1: "))
-operator = input("Operator: ")
-calc2 = float(input("Number2: "))
+play = True
 
-if operator == "*":
-    result1 = calc1 * calc2
-    print(round(result1, 2))
+while play:
     
-elif operator == "/":
-    result2 = calc1 / calc2
-    print(round(result2, 2))
+    calc1 = float(input("Number1: "))
+    operator = input("Operator: ")
+    calc2 = float(input("Number2: "))
     
-elif operator == "+":
-    result3 = calc1 + calc2
-    print(round(result3, 2))
+    if operator == "*":
+        result1 = calc1 * calc2
+        print(round(result1, 2))
+        
+    elif operator == "/":
+        result2 = calc1 / calc2
+        print(round(result2, 2))
+        
+    elif operator == "+":
+        result3 = calc1 + calc2
+        print(round(result3, 2))
+        
+    elif operator == "-":
+        result4 = calc1 - calc2
+        print(round(result4, 2))
+        
+    else:
+        print("Error! Invalid operator.")
+        break
+
+    restart = input("Restart? (y/n)" )
     
-elif operator == "-":
-    result4 = calc1 - calc2
-    print(round(result4, 2))
+    if restart == "y" or restart == "Y":
+        continue
     
-else:
-    print("Error! Try again.")
+    elif restart == "n" or restart == "N":
+        play = False
+        break
+    
+    else:
+        break
+        
+input("Press Enter to terminate...")
